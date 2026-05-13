4c867160de762ba8d03abba3c9fa9e34c40b996b
Muhammad Khoirul Anam <mkhoirulanam920@gmail.com>
2026-05-13 16:46:51 +0700

    feat: polish UI/UX, fix blog images, and implement social brand icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";  
 
 export default defineConfig({
   site: 'https://tropicroastery.com',
   integrations: [
+    icon(),
     sitemap()
   ]
 });
